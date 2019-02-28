06b9437a9b518e00a289aca7c4940df75fa51406
Benjamin Hallquist <benhall84707@gmail.com>
2019-02-28 11:26:08 -0500

    added click function

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// When we click...
+// When we click any .picture-frame
+// we see a large version of THAT image.
+
+// const means that it CANNOT BE REASSIIGGNNEDDD
+const targetElements = document.querySelectorAll("[data-target]");
+
+
+targetElements[0].addEventListener("click", ClickResponseSUPER1000version2);
+function ClickResponseSUPER1000version2() {
+    console.log("OMG ITS AMAAAAZIZINGGGGG");
+    console.log("YAAAAAA IM ALLIIIIVEEEE");
+}
