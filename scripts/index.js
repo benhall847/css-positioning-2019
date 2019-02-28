243ca64d8fa5426c84e06b3b9d6968609b69c934
Benjamin Hallquist <benhall84707@gmail.com>
2019-02-28 11:59:28 -0500

    added big class functionality

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,17 @@
 // const means that it CANNOT BE REASSIIGGNNEDDD
 const targetElements = document.querySelectorAll("[data-target]");
 
-// for (var i = 0; i <= (targetElements.length - 1); i++){
-
-
-// }
-function ClickResponseSUPER1000version2() {
+for (var i = 0; i <= (targetElements.length - 1); i++){
+}
+function ClickResponseSUPER1000version2(event) {
     console.log("OMG ITS AMAAAAZIZINGGGGG");
     console.log("YAAAAAA IM ALLIIIIVEEEE");
+    console.log(event.target.parentElement);
+    event.target.parentElement.classList.add('big');
 }
 
+
+// add the "big" class to the thing that got clicked
 
 function attachClickHandler(oneElement) {
     oneElement.addEventListener("click", ClickResponseSUPER1000version2);
